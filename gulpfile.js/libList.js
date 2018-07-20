e9e97179d8020c51b73545e1bd9afaf24bc7e972
Artem Grachov <grachov2195@gmail.com>
2018-07-20 16:51:28 +0300

    About us block. Gallery block. Gallery carousel

--- gulpfile.js/libList.js
+++ gulpfile.js/libList.js
@@ -1,4 +1,4 @@
 module.exports = {
-    jsLibs: ["jquery/dist/jquery.min.js"],
-    cssLibs: ["normalize.css/normalize.css"]
+    jsLibs: ["jquery/dist/jquery.min.js", "slick-carousel/slick/slick.min.js"],
+    cssLibs: ["normalize.css/normalize.css", "slick-carousel/slick/slick.css"]
 }
---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+$('.gallery-list').slick({
+  centerMode: true,
+  centerPadding: '30px',
+  slidesToShow: 3,
+  prevArrow: $('.gallery-arrows__btn_left'),
+  nextArrow: $('.gallery-arrows__btn_right'),
+});
