4eeab61bfe116fa3ab8b13f86541d846313e92eb
Artem Grachov <grachov2195@gmail.com>
2018-07-23 10:03:51 +0300

    Testimonials block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,13 @@ $('.gallery-list').slick({
   centerMode: true,
   centerPadding: '30px',
   slidesToShow: 3,
-  touchThreshold: 20,
+  touchThreshold: 50,
   prevArrow: $('.gallery-arrows__btn_left'),
   nextArrow: $('.gallery-arrows__btn_right'),
+});
+
+$('.testimonials-list').slick({
+  touchThreshold: 20,
+  prevArrow: $('.testimonials__slider-toggle_left'),
+  nextArrow: $('.testimonials__slider-toggle_right'),
 });
