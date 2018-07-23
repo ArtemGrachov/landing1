cf0f3f8a1fb804dd9e751c1c56be4551c40704d7
Artem Grachov <grachov2195@gmail.com>
2018-07-23 09:12:18 +0300

    Formatting for common html tags

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ $('.gallery-list').slick({
   centerMode: true,
   centerPadding: '30px',
   slidesToShow: 3,
+  touchThreshold: 20,
   prevArrow: $('.gallery-arrows__btn_left'),
   nextArrow: $('.gallery-arrows__btn_right'),
 });
